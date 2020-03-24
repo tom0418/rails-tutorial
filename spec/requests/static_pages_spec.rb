e377d96dcbefa1e7682591e07ee97a6efe824db1
Tomoya Ikegami <60150746+tom0418@users.noreply.github.com>
2020-03-24 22:24:17 +0900

    [add]static_pages: aboutページとrequest specを追加

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,9 +2,23 @@ require 'rails_helper'
 
 RSpec.describe 'StaticPages', type: :request do
   describe 'Home Page' do
-    it "should have the content 'Sample APP'" do
-      visit 'static_pages/home'
-      expect(page).to have_content('Sample APP')
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+  end
+
+  describe 'Help Page' do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+  end
+
+  describe 'About Page' do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
     end
   end
 end
