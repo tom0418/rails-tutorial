4152d2bca680fdfa02d4e7a96360b00df6b2d6aa
Tomoya Ikegami <60150746+tom0418@users.noreply.github.com>
2020-03-27 02:30:39 +0900

    [update]signupページのスタブとリクエストスペック修正

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe "UserPages", type: :request do
-  describe "GET /user_pages" do
-    it "works! (now write some real specs)" do
-      get user_pages_index_path
-      expect(response).to have_http_status(200)
-    end
+RSpec.describe 'User pages', type: :request do
+  subject { page }
+
+  describe 'Signup page' do
+    before { visit signup_path }
+
+    it { is_expected.to have_content('Sign up') }
+    it { is_expected.to have_title(full_title('Sign up')) }
   end
 end
