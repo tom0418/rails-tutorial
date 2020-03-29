fbf3d4db649b0f1b5047a46087c82c450fe27252
Tomoya Ikegami <60150746+tom0418@users.noreply.github.com>
2020-03-29 17:40:44 +0900

    [update]DB保存前のメールアドレスの一意性を保証する処理を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
 
+  before_save { self.email = email.downcase }
+
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 end
