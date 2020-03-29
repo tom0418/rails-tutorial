57d184e25766158bea63daecec71c71f7c39f767
Tomoya Ikegami <60150746+tom0418@users.noreply.github.com>
2020-03-29 17:55:24 +0900

    [update]email: 長さのバリデーションとRSpecを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
 
   before_save { self.email = email.downcase }
 
-  validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :name, presence: true,
+                   length: { maximum: 50 }
+  validates :email, presence: true,
+                    length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,9 +26,14 @@ RSpec.describe User, type: :model do
       it { is_expected.to be_invalid }
     end
 
+    describe 'when email is too long' do
+      before { user.email = 'a' * 244 + '@example.com' }
+      it { is_expected.to be_invalid }
+    end
+
     describe 'when email format is invalid' do
       it 'should be invalid' do
-        addresses = %w[user@foo,com user_at_foo.org example.user@foo.foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+        addresses = %w[user@foo,com user_at_foo.org example.user@foo.foo@bar.com foo@bar+baz.com foo@bar..com]
         addresses.each do |invalid_address|
           user.email = invalid_address
           expect(user).to be_invalid
