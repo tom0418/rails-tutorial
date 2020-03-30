cd1a9b05bcc7d9f4b7f06e7e461a4932b138a058
Tomoya Ikegami <60150746+tom0418@users.noreply.github.com>
2020-03-30 23:49:04 +0900

    [add]GET /users/:idの仮ページとリクエストスペック追加

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,6 +3,14 @@ require 'rails_helper'
 RSpec.describe 'User pages', type: :request do
   subject { page }
 
+  describe 'Profile page' do
+    let(:user) { FactoryBot.create(:user) }
+    before { visit user_path(user) }
+
+    it { is_expected.to have_content(user.name) }
+    it { is_expected.to have_title(user.name) }
+  end
+
   describe 'Signup page' do
     before { visit signup_path }
 
